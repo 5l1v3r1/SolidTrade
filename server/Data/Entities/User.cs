cb331b37982c800f6aa94040000fda60993e5704
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2021-12-05 16:15:50 +0100

    Initialize project

---
+++ server/Data/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using SolidTradeServer.Data.Entities.Common;
+
+namespace SolidTradeServer.Data.Entities
+{
+    public class User : BaseEntity
+    {
+        public string DisplayName { get; set; }
+        public string Username { get; set; }
+    }
+}
